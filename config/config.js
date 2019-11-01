e1f9aa38fd9d1b321071d0f02fa49dcdb8ca483f
dariamikl <d.miklashevskaya@innopolis,ru>
2019-11-01 22:15:03 +0300

    docker

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   url: 'http: //localhost',
   port: process.env.PORT || 3030,
   originUrl: 'http://localhost:3030',
-  mongoUrl: 'mongodb://localhost:27017/chat-app',
+  mongoUrl: 'mongodb://mongodb-replicaset-0.mongodb-replicaset.default.svc.cluster.local:27017/db',
   ISDEV: process.env.NODE_ENV !== 'production',
   emailer: {
     from: "Chat App <no-reply@chatapp.com>",
